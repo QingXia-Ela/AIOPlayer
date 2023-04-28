7854e8e75a4fc159d00b8d1b421614c9ed8abb35
QingXia-Ela <Elahere@163.com>
2023-04-28 21:58:29 +0800

    chore: add storybook env

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,30 +1,23 @@
 /* eslint-disable no-undef */
 module.exports = {
-    env: {
-        browser: true,
-        es2021: true,
-    },
-    extends: [
-        "eslint:recommended",
-        "plugin:react/recommended",
-        "plugin:@typescript-eslint/recommended",
-    ],
-    overrides: [],
-    parser: "@typescript-eslint/parser",
-    parserOptions: {
-        ecmaVersion: "latest",
-        sourceType: "module",
-    },
-    plugins: [
-        "react",
-        "@typescript-eslint"
-    ],
-    rules: {
-        "react/react-in-jsx-scope": "off",
-        "react/jsx-no-target-blank": "off",
-        "react/prop-types": "off",
-        "react/display-name": "off",
-        "@typescript-eslint/no-empty-interface": "warn",
-        "no-case-declarations": "off"
-    },
+  env: {
+    browser: true,
+    es2021: true
+  },
+  extends: ["eslint:recommended", "plugin:react/recommended", "plugin:@typescript-eslint/recommended", "plugin:storybook/recommended"],
+  overrides: [],
+  parser: "@typescript-eslint/parser",
+  parserOptions: {
+    ecmaVersion: "latest",
+    sourceType: "module"
+  },
+  plugins: ["react", "@typescript-eslint"],
+  rules: {
+    "react/react-in-jsx-scope": "off",
+    "react/jsx-no-target-blank": "off",
+    "react/prop-types": "off",
+    "react/display-name": "off",
+    "@typescript-eslint/no-empty-interface": "warn",
+    "no-case-declarations": "off"
+  }
 };
