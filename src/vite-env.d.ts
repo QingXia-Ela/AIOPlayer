4d88bdebb7d2f6755c6fb0b2f3a4c92641813b63
QingXia-Ela <Elahere@163.com>
2023-05-06 15:06:41 +0800

    chore: update deps

--- src/vite-env.d.ts
+++ src/vite-env.d.ts
@@ -1,1 +1,11 @@
 /// <reference types="vite/client" />
+
+interface TAURIWindow extends (typeof window) {
+  __TAURI_IPC__: any
+}
+
+declare global {
+  interface Window {
+    __TAURI_IPC__: any
+  }
+}
