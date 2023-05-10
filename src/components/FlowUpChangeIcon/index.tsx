6a8e6c54deca85ae0278d63ddf0db56cacdeeb32
QingXia-Ela <Elahere@163.com>
2023-05-10 21:44:32 +0800

    feat: add flow up change icon component

---
+++ src/components/FlowUpChangeIcon/index.tsx
@@ -0,0 +1,22 @@
+import { FunctionComponent, HTMLProps, useMemo } from "react";
+import Styles from './index.module.scss'
+
+interface FlowUpChangeIconProps extends HTMLProps<HTMLDivElement> {
+  IconMap: Record<string, JSX.Element>
+  currentIcon?: string
+}
+
+const FlowUpChangeIcon: FunctionComponent<FlowUpChangeIconProps> = ({ IconMap, currentIcon, ...p }) => {
+
+  const MemoIconMapElement = useMemo(() => Object.entries(IconMap).map(([k, v]) => (
+    <div key={k} className={`${Styles.icon} ${currentIcon === k ? Styles.active : Styles.inactive}`}>{v}</div>
+  )), [IconMap, currentIcon])
+
+  return (
+    <div {...p} className={`${Styles.wrapper} ${p.className || ""}`}>
+      {MemoIconMapElement}
+    </div>
+  );
+}
+
+export default FlowUpChangeIcon;
